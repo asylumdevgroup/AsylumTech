65aaa6871a0611ea0438eb0c5826adf35d3a1782
mcadventurecity <legodata@comcast.net>
2023-07-23 14:54:49 -0400

    Setup project

---
+++ src/main/java/com/asylumdev/asylumtech/Tags.java
@@ -0,0 +1,7 @@
+package com.asylumdev.asylumtech;
+
+public class Tags {
+    public static final String MODID = "asylumtech";
+    public static final String MODNAME = "AsylumTech";
+    public static final String VERSION = "0.0.1";
+}
